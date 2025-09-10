e15dd9275465de0accf6ddb2c005cf1687faa7f4
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-09-10 15:31:37 +0530

    Set up pose detection with a single marker instead of 4

--- aruco_screen.py
+++ aruco_screen.py
@@ -2,10 +2,7 @@ import cv2
 import numpy as np
 
 aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-corner_ids = [0, 1, 2, 3]
-
 aruco_params = cv2.aruco.DetectorParameters()
-
 
 canvas_width, canvas_height = 800, 600
 
@@ -20,7 +17,7 @@ overlay_coordinates = [
 
 for overlay in overlay_coordinates:
     start, end = overlay
-    cv2.rectangle(canvas, start, end, (0, 255, 255), 2)
+    cv2.rectangle(canvas, start, end, (0, 0, 255), -1)
 
 src_pts = np.array([
     [0, 0],
@@ -33,6 +30,8 @@ src_pts = np.array([
 cap = cv2.VideoCapture(0)
 
 frame_height, frame_width = None, None
+
+warped_canvas = None
 
 while True:
     ret, frame = cap.read()
@@ -47,25 +46,25 @@ while True:
     if ids is not None:
         ids = ids.flatten()
         dst_pts = np.zeros((4, 2), dtype=np.float32)
-        all_detected = True  # flag to know if we have all 4 markers
 
-        for i, marker_id in enumerate([0, 1, 2, 3]):  # TL, TR, BR, BL
-            matches = np.where(ids == marker_id)[0]
-            if matches.size > 0:
-                index = matches[0]
-                dst_pts[i] = corners[index][0][0]
-            else:
-                print(f"Marker {marker_id} not detected in this frame")
-                all_detected = False
+        print(corners)
 
-        if all_detected:
-            print(dst_pts)
+        if 0 in ids:
+            dst_pts[0] = corners[0][0][0]
+            dst_pts[1] = corners[0][0][1]
+            dst_pts[2] = corners[0][0][2]
+            dst_pts[3] = corners[0][0][3]
+
             H, _ = cv2.findHomography(src_pts, dst_pts)
             warped_canvas = cv2.warpPerspective(canvas, H, (frame_width, frame_height))
             warped_canvas = cv2.resize(warped_canvas, (frame_width, frame_height))
             mask = np.any(warped_canvas != 0, axis=2)  # True where at least one channel is non-black
-            for c in range(3):  # BGR channels
-                frame[:, :, c][mask] = warped_canvas[:, :, c][mask]
+
+    if warped_canvas is not None:
+        for c in range(3):  # BGR channels
+            frame[:, :, c][mask] = warped_canvas[:, :, c][mask]
 
     cv2.imshow("dbhs", frame)
-    cv2.waitKey(2)
+
+    if cv2.waitKey(2) & 0xFF == ord('q'):
+        break
