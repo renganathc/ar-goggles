2c531fb049c5c7c44299812d081d81f4b7bd2be6
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-10-01 07:21:46 +0530

    Implemented a basic jumping dino using PyGame

---
+++ dino.py
@@ -0,0 +1,59 @@
+import cv2
+import pygame
+import numpy as np
+import random
+
+pygame.init()
+cap = cv2.VideoCapture(0)
+frame_rate = 30
+
+ret, frame = cap.read()
+h, w, _ = frame.shape
+h = h * 5 // 7
+w = w * 5 // 7
+screen = pygame.display.set_mode((w, h))
+
+dino_y = h - 100
+dino_vel = 0
+gravity = 5
+is_jumping = False
+
+def jump_fn():
+    global dino_vel, is_jumping
+    if not is_jumping:
+        dino_vel = -34
+        is_jumping = True
+
+clock = pygame.time.Clock()
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_SPACE]:  # space triggers jump
+        jump_fn()
+    
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (w, h))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = np.rot90(frame)
+    frame_surface = pygame.surfarray.make_surface(frame)
+
+    dino_y += dino_vel
+    dino_vel += gravity
+    if dino_y >= h - 100:  # ground level
+        dino_y = h - 100
+        is_jumping = False
+    
+    screen.blit(frame_surface, (0,0))
+    pygame.draw.rect(screen, (0,255,0), (50, dino_y, 50, 50))
+
+    pygame.display.update()
+    clock.tick(frame_rate)
+
+cap.release()
+pygame.quit()  
