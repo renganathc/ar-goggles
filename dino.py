a876c652379ebd5d905af3b83136cfe9065b3edc
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-10-03 18:32:41 +0530

    feat: Implemented adaptive difficulty level in the game

--- dino.py
+++ dino.py
@@ -7,32 +7,36 @@ pygame.init()
 cap = cv2.VideoCapture(0)
 frame_rate = 30
 
+# Get video feed size
 ret, frame = cap.read()
 h, w, _ = frame.shape
 h = h * 5 // 7
 w = w * 5 // 7
 screen = pygame.display.set_mode((w, h))
 
+# Dino properties
 dino_y = h - 100
 dino_vel = 0
-gravity = 5
+gravity = 4
 is_jumping = False
 
 obstacles = []  # list of dicts: {'x', 'y', 'w', 'h'}
 obstacle_speed = 17
 spawn_timer = 0
 
+dist_cov = 0
+min_dist, max_dist = 30, 50 # next block
+
 def jump_fn():
     global dino_vel, is_jumping
     if not is_jumping:
-        dino_vel = -34
+        dino_vel = -34   # jump strength
         is_jumping = True
 
 clock = pygame.time.Clock()
 running = True
 
 while running:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -57,7 +61,7 @@ while running:
     if spawn_timer <= 0:
         obs_h = random.randint(75, 167)
         obstacles.append({'x': w, 'y': h - obs_h, 'w': 20, 'h': obs_h})
-        spawn_timer = random.randint(40, 90)  # next spawn
+        spawn_timer = random.randint(min_dist, max_dist)  # next spawn
 
     for obs in obstacles:
         obs['x'] -= obstacle_speed
@@ -65,12 +69,20 @@ while running:
     obstacles = [o for o in obstacles if o['x'] + o['w'] > 0]
     
     screen.blit(frame_surface, (0,0))
-    pygame.draw.rect(screen, (0,255,0), (50, dino_y, 50, 50))
+    pygame.draw.rect(screen, (0,255,0), (50, dino_y, 50, 50))  # green dino
     for obs in obstacles:
         pygame.draw.rect(screen, (255,0,0), (obs['x'], obs['y'], obs['w'], obs['h']))
 
     pygame.display.update()
     clock.tick(frame_rate)
+    dist_cov += 1
+
+    if dist_cov % 120 == 0:
+        obstacle_speed += 2
+        if min_dist > 19:
+            min_dist -= 2
+        if max_dist > 20:
+            max_dist -= 2
 
 cap.release()
 pygame.quit()  
