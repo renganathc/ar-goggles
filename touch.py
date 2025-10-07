4a28c617122671dc3b0a5c46f15a6d0a5f617b7b
Renganath Chokkalingam <rengnath1234@gmail.com>
2025-10-07 21:57:23 +0530

    Created method to detect jump gesture for the dino game

--- touch.py
+++ touch.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import math
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
@@ -30,3 +29,23 @@ def Touch(frame, H, element_coordinates, canvas, hands_method):
 					break
 
 	return canvas,element
+
+def jump_gest_detector(frame, hands_method):
+	frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2RGB)
+	result = hands_method.process(frame)
+	h, w = frame.shape[:2]
+	jump_gesture_detected = False
+
+	if result.multi_hand_landmarks:
+		hand_landmarks = result.multi_hand_landmarks[0]
+		index = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+		thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP]
+
+		ix, iy = int(index.x * w), int(index.y * h)
+		tx, ty = int(thumb.x * w), int(thumb.y * h)
+
+		distance = ((ix - tx)**2 + (iy - ty)**2)**0.5
+		if distance < int(h/11.5): # sensitivity parameter :)
+			jump_gesture_detected = True
+
+	return jump_gesture_detected
